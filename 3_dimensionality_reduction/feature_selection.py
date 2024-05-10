028aaaf6565eb9d219d2233452fa7abcc54f0c05
maerdian01 <maerdian01@uni-koblenz.de>
2024-05-10 13:13:41 +0200

    determined top 10 most relevant features by chi2

--- 3_dimensionality_reduction/feature_selection.py
+++ 3_dimensionality_reduction/feature_selection.py
@@ -20,20 +20,42 @@ def select_features(df, k):
 
     return df_chi2
 
+# select the top 10 most 
+def select_top10(df):
+    # divide into predictor (X) and response (y) varibales
+    X = df.drop('_class_', axis=1)
+    y = df['_class_']
+
+    # create and fit selector
+    selector = SelectKBest(chi2, k=10)
+    selector.fit(X, y)
+
+    # map features to their chi2 scores, sort them by score and retrieve the top 10
+    features_scores_map = { X.columns[index]: selector.scores_[index] for index, value in enumerate(X.columns) }
+    top10_feature_names = sorted(features_scores_map, key=features_scores_map.get, reverse=True)[:10]
+
+    print('Top 10 most relevant features:', top10_feature_names)
+
 # perform feature selection on requirements data with parameters for k determined in hyperparameter_tuning.py for each classifier
 
-# select_features(df_bow, 450).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_bow_knn.csv', index=False)
-# select_features(df_tfidf, 850).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_tfidf_knn.csv', index=False)
+select_features(df_bow, 450).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_bow_knn.csv', index=False)
+select_features(df_tfidf, 850).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_tfidf_knn.csv', index=False)
 
-# select_features(df_bow, 275).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_bow_svm.csv', index=False)
-# select_features(df_tfidf, 725).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_tfidf_svm.csv', index=False)
+select_features(df_bow, 275).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_bow_svm.csv', index=False)
+select_features(df_tfidf, 725).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_tfidf_svm.csv', index=False)
 
-# select_features(df_bow, 150).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_bow_lr.csv', index=False)
-# select_features(df_tfidf, 750).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_tfidf_lr.csv', index=False)
+select_features(df_bow, 150).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_bow_lr.csv', index=False)
+select_features(df_tfidf, 750).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_tfidf_lr.csv', index=False)
 
-# select_features(df_bow, 550).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_bow_mnb.csv', index=False)
-# select_features(df_tfidf, 675).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_tfidf_mnb.csv', index=False)
+select_features(df_bow, 550).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_bow_mnb.csv', index=False)
+select_features(df_tfidf, 675).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_tfidf_mnb.csv', index=False)
 
 select_features(df_bow, 700).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_bow_ensemble.csv', index=False)
 select_features(df_tfidf, 825).to_csv('3_dimensionality_reduction/output/req_chi2/req_chi2_tfidf_ensemble.csv', index=False)
 
+# Top 10
+print('BoW:')
+select_top10(df_bow)
+
+print('TF-IDF:')
+select_top10(df_tfidf)
