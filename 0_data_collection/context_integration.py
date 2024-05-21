6552e3dd76c40155b54ed93de791acada5807d21
maerdian01 <maerdian01@uni-koblenz.de>
2024-05-21 20:51:56 +0200

    building the lookup table for terms and their contexts

---
+++ 0_data_collection/context_integration.py
@@ -0,0 +1,123 @@
+import numpy
+import pandas as pd 
+import re
+
+# read terms and abbreviations excel files and convert them into dataframe objects
+df_terms = pd.DataFrame(pd.read_excel('resources/ECSS-Definitions_active-and-superseded-Standards-(from-ECSS-DOORS-database-v0.9_5Oct2022).xlsx')) 
+df_abbreviations = pd.DataFrame(pd.read_excel('resources/ECSS-Abbreviated-Terms_active-and-superseded-Standards-(from-ECSS-DOORS-database-v0.9_5Oct2022).xlsx', na_values=['NULL', 'null'], keep_default_na=False))
+# remove unnecessary columns
+df_terms = df_terms.drop(['Definition #', 'Context', 'Defintion text', 'Note'], axis=1)
+df_abbreviations = df_abbreviations.drop(['Meaning'], axis=1)
+
+# remove abbreviations nested in parentheses from df_terms
+# the abbreviations are listed in df_abbreviations
+df_terms['Term'] = df_terms['Term'].apply(
+    lambda x: re.sub('[\(].*?[\)]', '', str(x)).strip())
+
+# add the branch as context information to each term 
+def add_branches(df : pd.DataFrame):
+    for index, row in df.iterrows():
+        standard = row['ECSS Standard']
+        df.at[index, 'Branch']=standard[5]
+
+# adding branches
+add_branches(df_terms)
+add_branches(df_abbreviations)
+
+# build the lookup table for terms and abbreviations with their belonging contexts 
+def build_lookup_table():
+    df_lookup = pd.DataFrame(columns=['Term', 'ECSS Standards', 'Branches'])
+
+    # iterate over all terms
+    for index, row in df_terms.iterrows():
+        term = row['Term']
+        standard = row['ECSS Standard']
+        branch = row['Branch']
+        
+        if term not in df_lookup['Term'].tolist():
+            df_lookup = df_lookup._append({'Term': term, 'ECSS Standards': [standard], 'Branches': [branch]}, ignore_index=True)
+        else:
+            # add the standard to the term's list of standards
+            standards = df_lookup.loc[df_lookup['Term'] == term, 'ECSS Standards'].values[0]
+            if standard not in standards: 
+                standards.append(standard)
+            # add the branch to the term's list of branches
+            branches = df_lookup.loc[df_lookup['Term'] == term, 'Branches'].values[0]
+            if branch not in branches: 
+                branches.append(branch)
+    
+    # iterate over all abbreviations
+    for index, row in df_abbreviations.iterrows():
+        term = row['Abbreviation']
+        standard = row['ECSS Standard']
+        branch = row['Branch']
+        
+        if row['Active or Superseded Standard'] == 'superseded':
+            continue
+        if term not in df_lookup['Term'].tolist():
+            df_lookup = df_lookup._append({'Term': term, 'ECSS Standards': [standard], 'Branches': [branch]}, ignore_index=True)
+        else:
+            # add the standard to the term's list of standards
+            standards = df_lookup.loc[df_lookup['Term'] == term, 'ECSS Standards'].values[0]
+            if standard not in standards: 
+                standards.append(standard) 
+            # add the branch to the term's list of branches
+            branches = df_lookup.loc[df_lookup['Term'] == term, 'Branches'].values[0]
+            if branch not in branches: 
+                branches.append(branch)
+
+    return df_lookup
+
+# building the lookup table
+df_lookup = build_lookup_table()
+df_lookup.to_csv('0_data_collection/output/lookup.csv', index=False)
+
+
+# import requirements data without context information
+df_req = pd.read_csv(filepath_or_buffer='0_data_collection/output/ECSS_standards.csv', header=0, quotechar='"', doublequote=True)
+
+# add context information to the requirements data
+def extend_by_context(df : pd.DataFrame):
+    for index, row in df.iterrows():
+        text = row['RequirementText']
+        standard = row['ECSS Source Reference']
+        contexts = find_contexts_in_text(text, standard, standard[5])
+        for context in contexts:
+            df.at[index, context] = 1
+
+# find terms with context information in a requirement text belonging to a certain standard and return them
+def find_contexts_in_text(text : str, standard : str, branch : str):
+    contexts = list()
+    
+    # check list of full terms
+    for index, row in df_terms.iterrows():
+        term = row['Term']
+        standards = row['ECSS Standards']
+        branches = row['Branches']
+        if re.search(term, text, re.IGNORECASE):
+            if standard in standards:
+                contexts.append('context_' + term + '_' + standard) 
+            elif branch in branches:
+                contexts.append('context_' + term + '_' + branch)
+            elif 'ECSS-S-ST-00-01C' in standards: 
+                contexts.append('context_' + term + '_' + 'ECSS')
+
+    # check list of abbreviations
+    for index, row in df_abbreviations.iterrows():
+        term = row['Term']
+        standards = row['ECSS Standards']
+        branches = row['Branches']
+        if re.search(term, text, re.IGNORECASE):
+            if standard in standards:
+                contexts.append('context_' + term + '_' + standard) 
+            elif branch in branches:
+                contexts.append('context_' + term + '_' + branch)
+            else: 
+                contexts.append('context_' + term + '_' + 'ECSS')
+
+    return contexts
+
+
+print(df_terms)
+print(df_abbreviations)
+print(df_req)
--- 0_data_collection/data_collection.py
+++ 0_data_collection/data_collection.py
@@ -3,14 +3,14 @@ import pandas as pd
 # read EARM excel file and convert into a dataframe object 
 df = pd.DataFrame(pd.read_excel('resources/EARM_ECSS_export(DOORS-v0.9_v2_21Feb2023).xlsx')) 
 # remove unnecessary columns
-df_classes = df.drop(['ECSS Source Reference', 'DOORS Project', 'ECSS Req. Identifier', 'Type', 
+df_classes = df.drop(['DOORS Project', 'ECSS Req. Identifier', 'Type', 
               'RCM Version', 'ECSS Change Status', 'Text of Note of Original requirement', 
               'Klasse', 'Anzahl', '_subclass_'], axis=1)
 df_subclasses = df.drop(['ECSS Source Reference', 'DOORS Project', 'ECSS Req. Identifier', 'Type', 
               'RCM Version', 'ECSS Change Status', 'Text of Note of Original requirement', 
               'Klasse', 'Anzahl', '_class_'], axis=1)
 # rename columns
-df_classes.columns = ['ID', 'RequirementText', '_class_']
+df_classes.columns = ['ECSS Source Reference', 'ID', 'RequirementText', '_class_']
 df_subclasses.columns = ['ID', 'RequirementText', '_subclass_']
 
 # remove new lines from the requirement texts
